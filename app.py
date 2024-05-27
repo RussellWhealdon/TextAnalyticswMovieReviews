236a8859bb03555aa315a851c619475c4d78df83
RussellWhealdon <120282073+RussellWhealdon@users.noreply.github.com>
2024-05-27 18:01:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ def main():
         # Show raw movie data
         if st.checkbox("Show Raw Movie Data"):
             st.write(df_movies)
-            poster_path = df_movies.iloc[7]['poster_path']
-            display_movie_poster(poster_path)
+        poster_path = df_movies.iloc[7]['poster_path']
+        display_movie_poster(poster_path)
 
 # Run the app
 if __name__ == "__main__":
